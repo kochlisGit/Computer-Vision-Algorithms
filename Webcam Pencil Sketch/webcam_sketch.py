9115ca4dba2f2b72038a85cc95cd96c7a20d772d
Vasileios Kochliaridis <kohliaridis@Hotmail.com>
2020-10-20 14:50:59 +0300

    Update webcam_sketch.py

--- Webcam Pencil Sketch/webcam_sketch.py
+++ Webcam Pencil Sketch/webcam_sketch.py
@@ -1,31 +1,56 @@
-import cv2
-import numpy as np
+from cv2 import VideoCapture, waitKey , destroyAllWindows
+from cv2 import imwrite, imshow
+from cv2 import cvtColor, COLOR_RGB2GRAY, COLOR_GRAY2RGB
+from cv2 import GaussianBlur, Canny, threshold, THRESH_BINARY_INV
+from cv2 import divide
 
-ESC = 27
-ENTER = 13
+KEY_1 = 49
+KEY_2 = 50
+KEY_ESC = 27
+KEY_ENTER = 13
 
 # Create a color dodge, which lightens the bottom layer of the image.
 def dodge(input_image, mask):
-    return cv2.divide(input_image, 255 - mask, scale=256)
+    return divide(input_image, 255 - mask, scale=256)
 
-# Returns a sketch of the image. Uses Gaussian Thresholding.
-def sketch(input_image):
+# 1st Filter:
+# Returns a pencil sketch of the image. Uses Dodge + Gaussian Blur.
+def sketch_f1(input_image):
     # 1. Convert the RGB image from webcame to Grayscale.
-    gray_image = cv2.cvtColor(input_image, cv2.COLOR_RGB2GRAY)
+    gray_image = cvtColor(input_image, COLOR_RGB2GRAY)
 
     # 2. Invert the grayscaled image.
     inverted_gray_image = 255 - gray_image
 
     # 3. Apply Gaussian blur to the inverted grayscaled image.
-    blurred_inverted_gray_image = cv2.GaussianBlur(inverted_gray_image, ksize=(21, 21), sigmaX=0)
+    blurred_inverted_gray_image = GaussianBlur(inverted_gray_image, ksize=(21, 21), sigmaX=0)
 
     # 4. Blend the original gray image with the blurred.
     blended_image = dodge(gray_image, blurred_inverted_gray_image)
 
-    return cv2.cvtColor(blended_image, cv2.COLOR_GRAY2RGB)
+    return cvtColor(blended_image, COLOR_GRAY2RGB)
 
+# 2nd Filter:
+# Returns a pencil sketch of the image. Uses Canny Edge Detection + Gaussian Thresholding.
+def sketch_f2(input_image):
+    # 1. Convert the RGB image from webcame to Grayscale.
+    gray_image = cvtColor(input_image, COLOR_RGB2GRAY)
+
+    # 2. Apply Gaussian blur to the grayscaled image.
+    blurred_image = GaussianBlur(gray_image, ksize=(5,5), sigmaX=0)
+
+    # 3. Applying Canny edge detection algorithm to the blurred image.
+    canny_edges = Canny(blurred_image, threshold1=10, threshold2=70)
+
+    # 4. Applying binary thresholding.
+    _, mask = threshold(canny_edges, 70, 255, THRESH_BINARY_INV)
+    return mask
+    
 # Initializing Capture
-webcam = cv2.VideoCapture(0)
+webcam = VideoCapture(0)
+
+# Initializing default filter.
+sketch = sketch_f1
 
 # Main Loop
 key = 0
@@ -33,12 +58,16 @@ while True:
     ret, input_frame = webcam.read()
     if ret:
         webcam_sketch = sketch(input_frame)
-        cv2.imshow('Webcam Sketch', webcam_sketch)
-    key = cv2.waitKey(1)
-    if key == ENTER:
-        cv2.imwrite('sketch.jpg', webcam_sketch)
-    if key == ESC:
+        imshow('Webcam Sketch', webcam_sketch)
+    key = waitKey(1)
+    if key == KEY_1:
+        sketch = sketch_f1
+    elif key == KEY_2:
+        sketch = sketch_f2
+    elif key == KEY_ENTER:
+        imwrite('sketch.jpg', webcam_sketch)
+    elif key == KEY_ESC:
         break
 
 webcam.release()
-cv2.destroyAllWindows()
+destroyAllWindows()
